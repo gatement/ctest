c88156e63b994758f048e0f88ac8e544e7c84dfc
Johnson Lau <gatement@gmail.com>
2015-12-24 22:01:07 +0800

    go

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+#include "util.h"
+
+int main(void)
+{
+   printf("ver: %s\n", get_ver()); 
+}
---
+++ util/util.c
@@ -0,0 +1,6 @@
+#include "util.h"
+
+const char* get_ver(void)
+{
+    return "v0.1.0";
+}
